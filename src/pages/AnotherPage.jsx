95b76d0cf30115ce5eb0d67b21e9c439354fa77d
Vignesh D <vignesh.d9123@gmail.com>
2025-06-01 13:58:28 +0530

    add dynamic content to another page comp

--- src/pages/AnotherPage.jsx
+++ src/pages/AnotherPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 function AnotherPage() {
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setData(data))
+  }, [])
   return (
     <div>
       <h1>Another Page</h1>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   )
 }
